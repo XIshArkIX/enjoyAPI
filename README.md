90eabbf9484bcf90f8aa4347a8101671556122f8
Roman <teleport1605@gmail.com>
2020-05-26 10:42:22 +0300

    ES6: arrow function + code improvements

--- README.md
+++ README.md
@@ -8,22 +8,27 @@ $ npm install sqdshcom/enjoyAPI --save
 
 ## Пример работы
 ```js
-const token = "тут-токен";
-const enjoyAPI = require("enjoyAPI");
+const token = 'тут-токен';
+const enjoyAPI = require('enjoyAPI');
 const client = new enjoyAPI(token);
 
-client.check("123456789012345678") // Проверить пользователя на наличие в ЧС проекта EnjoyMickey
-    .then(res =>
+client.check('123456789012345678') // Проверить пользователя на наличие в ЧС проекта EnjoyMickey
+    .then((res) =>
         console.log(res);
-        /* При отсутствии: {
-            userID: "123456789012345678",
-            active: false,
-            reason: null
-        }
-        При присутствии: {
-            userID: "123456789012345678",
-            active: true,
-            reason: "Очень плохой пользователь Discord"
-        } */
+        /*
+            При отсутствии: {
+                userID: '123456789012345678',
+                active: false,
+                reason: null
+            }
+            При присутствии: {
+                userID: '123456789012345678',
+                active: true,
+                reason: 'Очень плохой пользователь Discord'
+            }
+        */
+    )
+    .catch((err) =>
+        consolre.error(err) // ловим ошибку, если есть
     );
 ```
--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-/* jshint esversion: 6 */
-const path = "https://api.enjoymickeybot.info";
-const { request } = require("https");
-function send(url, token) {
+const path = 'https://api.enjoymickeybot.info';
+const { get } = require('https');
+const send = (url, token) => {
     return new Promise((resolve, reject) => {
-        let req = request(encodeURI(path + url), (res) => {
-            res.setEncoding('utf8');
-            res.on('data', (data) => resolve( JSON.parse(data) ));
-        });
+        const req = get(
+            encodeURI(path + url),
+            (res) => {
+                let data = [];
+                res.on('data', data = [...data, res]);
+                res.on('end', resolve(Buffer.concat(data).toString()));
+            }
+        );
 
-        req.setHeader("Authorization", token);
-        req.on('error', reject);
+        req.setHeader('Authorization', token);
+        req.on('error', reject(Error('Ошибка в GET запросе')));
         req.end();
     });
 }
 
 class EnjoyAPI {
     constructor(token) {
-        if(!token) return console.error("[EnjoyAPI] Не указан токен авторизации.");
+        if (!token)
+            throw '[EnjoyAPI] Не указан токен авторизации';
         this.token = token;
     }
 
-    check(userID) {
-        if(!userID) return console.error("[EnjoyAPI] Не указан ID пользователя для проверки.");
-        return new Promise((resolve) =>
+    check = (userID) => {
+        if(!userID)
+            return console.error('[EnjoyAPI] Не указан ID пользователя для проверки.');
+        return new Promise((resolve, reject) =>
             send(`/check/${userID}`, this.token)
-                .then(res => {
-                    if(![200, 404].includes(res.code)) return resolve({ error: res });
-                    return resolve({
-                        userID: userID,
-                        active: (res.code == 200) ? true : false,
-                        reason: (res.code == 200) ? res.reason : null
-                    });
+                .then((res) => {
+                    if(![200, 404].includes(res.code))
+                        return reject({ error: res });
+                    else
+                        return resolve({
+                            userID: userID,
+                            active: (res.code == 200) ? true : false,
+                            reason: (res.code == 200) ? res.reason : null
+                        });
                 })
+                .catch((err) => [console.error(err), reject(err)])
         );
     }
 }
